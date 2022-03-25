793e507cb10b65a6f6ded76f2efc43af2fc68156
Dhanesh kumar <dhanesh.bcss19@iba-suk.edu.pk>
2022-03-25 16:10:58 +0500

    applying image histogram & image streaching

---
+++ histogram.m
@@ -0,0 +1,141 @@
+function varargout = histogram(varargin)
+% HISTOGRAM MATLAB code for histogram.fig
+%      HISTOGRAM, by itself, creates a new HISTOGRAM or raises the existing
+%      singleton*.
+%
+%      H = HISTOGRAM returns the handle to a new HISTOGRAM or the handle to
+%      the existing singleton*.
+%
+%      HISTOGRAM('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in HISTOGRAM.M with the given input arguments.
+%
+%      HISTOGRAM('Property','Value',...) creates a new HISTOGRAM or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before histogram_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to histogram_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help histogram
+
+% Last Modified by GUIDE v2.5 10-Jan-2022 13:11:18
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @histogram_OpeningFcn, ...
+                   'gui_OutputFcn',  @histogram_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before histogram is made visible.
+function histogram_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to histogram (see VARARGIN)
+
+% Choose default command line output for histogram
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes histogram wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = histogram_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+a=uigetfile('*.*');
+a=imread(a);
+axes(handles.axes1);
+imshow(a);
+setappdata(0,'a',a);
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+X=getappdata(0,'a');
+imh=histeq(X);
+axes(handles.axes4);
+imshow(imh);
+setappdata(0,'imh',imh);
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+X=getappdata(0,'a');
+s=imadjust(X,[0.3,0.6],[0.0,1.0]);
+axes(handles.axes3);
+imshow(s);
+setappdata(0,'s',s);
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+assignment
+closereq
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+X=getappdata(0,'a');
+axes(handles.axes2);
+imhist(X);
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+X=getappdata(0,'s');
+axes(handles.axes5);
+imhist(X);
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+X=getappdata(0,'imh');
+axes(handles.axes6);
+imhist(X);
